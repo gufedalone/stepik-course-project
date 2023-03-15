59f367ebff5b51bd6ca738f330dfea7743757917
vlad <khalvlalove@gmail.com>
2023-03-15 14:16:35 +0400

    add locators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, 'p.price_color')
+    ADDED_PRODUCT_NAME = (By.CSS_SELECTOR, 'div.alert-success:first-child .alertinner strong')
+    CART_PRICE = (By.CSS_SELECTOR, '.alert-info .alertinner strong')
